3378ede963022e43bcb71a69692cfd376d849902
ImMavz <josephherrera1407@gmail.com>
2025-04-05 23:32:48 -0500

    Se corrige error con los estilos y el redireccionamiento.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,8 @@ import Carnes from './carnes.jsx'
 import Login from './login.jsx'
 import Aseo from './aseo.jsx'
 import FrutasVerduras from './frutasVerduras.jsx'
-import About from './About.jsx'
+import About from './about.jsx'
+import Categorias from './categorias.jsx'
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/aseo" element={<Aseo />} />
         <Route path ="frutasVerduras" element={<FrutasVerduras/>} />
         <Route path="/about" element={<About />} />
+        <Route path="/categorias" element={<Categorias />} />      
       </Routes>
     </Router>
   </StrictMode>,
